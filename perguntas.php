ebfd940aeb617d751397aa9471af6aa538052dcd
Eduardo Bona <eduardobona@vivaweb.net>
2018-07-25 17:19:08 -0300

    adicionando arquivo novo_index.php conforme copia do index.html com o include em perguntas.php e adicionando perguntas.php com a proposta de estrutura de arrays para ser desenvolvida no mapa

---
+++ perguntas.php
@@ -0,0 +1,17 @@
+<?php
+
+// TODO: atividade 1: estruturar o array de perguntas conforme hotsite
+// TODO: atividade 2: receber este array no site (novo_index.php) e deixar dinâmico
+
+$perguntas = array(
+    1 => array(
+        'titulo' => 'Titulo da pergunta 1',
+        'resposta' => 'Resposta da pergunta 1',
+        'imagem' => 'localizacao_da_imagem.jpg',
+        'fundo' => 'nome_do_fundo_para_mudar_por_bloco'
+    )
+    // continuar...
+);
+
+return $perguntas;
+
